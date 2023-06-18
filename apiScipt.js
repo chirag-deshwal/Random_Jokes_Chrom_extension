b10524c651ab8d17c85ca75b0c8ebdfe82741e5a
rahuldeshwal21 <info.dsd47@gmail.com>
2023-06-18 14:24:07 +0530

    Initial commit

---
+++ apiScipt.js
@@ -0,0 +1,25 @@
+var myJoke = "Nothing is here"
+
+
+fetch("https://icanhazdadjoke.com/slack")
+.then(data => data.json())
+.then(jokeData => {
+    const joke = jokeData.attachments[0].text;
+
+    
+    const jokeTxt = document.getElementById("jokeElement");
+ 
+ 
+   //save the joke 
+    myJoke = joke;
+
+    //Display Joke on html text
+    jokeTxt.innerHTML = joke;
+})
+
+
+
+
+
+
+
